d776b5154f6b7d2235283893dbf395ce5b058a09
Nadir9222 <nmokhtari785@gmail.com>
2023-10-02 16:51:37 +0200

    création de première solution de cryptage

---
+++ Cryptage.py
@@ -0,0 +1,20 @@
+import string
+
+def crypt(message):
+    caracteres = string.ascii_letters + string.punctuation + string.digits + " "
+    message_crypte = ""
+
+    for lettre in message:
+        if lettre in caracteres:
+            index = caracteres.index(lettre)
+            lettre_cryptee = caracteres[(index + 1) % len(caracteres)]
+            message_crypte += lettre_cryptee
+        else:
+            message_crypte += lettre
+
+    return message_crypte
+
+# Exemple d'utilisation :
+message = "fizzbuzz"
+message_crypte = crypt(message)
+print(message_crypte)
